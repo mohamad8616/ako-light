de41198c2891608b5a18400be60af98df05356ca
mohamad8616 <mohamad.sh.a9036@gmail.com>
2026-08-16 11:15:13 +0330

    add caption for hero section video

---
+++ components/about/AboutHeroVideo.tsx
@@ -0,0 +1,14 @@
+import HomepageSection from "../ui/HomepageSection";
+
+const AboutHeroVideo = () => {
+  return (
+    <HomepageSection className="flex items-center justify-start bg-background-secondary h-20 sm:h-24 md:h-30 lg:h-34
+    ">
+      <p className="font-jetbrains text-sm  font-light leading-normal text-gray-500 w-1/4  ms-12">
+        How Italians Sound: behind our products and beyond the Design industry.
+      </p>
+    </HomepageSection>
+  );
+};
+
+export default AboutHeroVideo;
