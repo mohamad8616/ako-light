095d68d3559d1e6f8f563593c80d4072d8f9c83c
mohamad8616 <mohamad.sh.a9036@gmail.com>
2026-08-31 11:07:10 +0330

    make font bolder, remove unnessecery use client

--- components/products/ProductsHeader.tsx
+++ components/products/ProductsHeader.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import PageTitle from "@/utility/PageTitle";
 
 interface ProductsHeaderProps {
@@ -9,7 +7,7 @@ interface ProductsHeaderProps {
 export default function ProductsHeader({ title = "Products" }: ProductsHeaderProps) {
   return (
     <header className="bg-background w-full pt-28 md:pt-76">
-      <PageTitle>{title}</PageTitle>
+      <PageTitle className="font-medium">{title}</PageTitle>
     </header>
   );
 }
